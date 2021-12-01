a119a2bca67516d6effb7547050ce9b276959551
Priyadharshini <priyadharshini.c@indiumsoft.com>
2021-12-01 18:47:28 +0530

    activity

---
+++ app/src/main/java/com/example/kotlinomnicure/activity/ExpirePasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinomnicure.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinomnicure.R
+
+class ExpirePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expire_password)
+    }
+}
---
+++ app/src/main/java/com/example/kotlinomnicure/apiRetrofit/ApiEndpoints.kt
@@ -0,0 +1,48 @@
+package com.example.kotlinomnicure.apiRetrofit
+
+import com.example.dailytasksamplepoc.kotlinomnicure.endpoints.healthcareEndPoints.Model.AddNotificationDataRequest
+import com.example.dailytasksamplepoc.kotlinomnicure.endpoints.healthcareEndPoints.Model.CommonResponseProviderNotification
+import com.mvp.omnicure.kotlinactivity.requestbodys.LoginDetailsRequestBody
+import com.mvp.omnicure.kotlinactivity.requestbodys.LogoutRequestBody
+import com.mvp.omnicure.kotlinactivity.requestbodys.ProviderNotificationDetailsRequestBody
+import com.mvp.omnicure.kotlinactivity.requestbodys.UpdateFcmkKeyRequestBody
+import omnicurekotlin.example.com.Model.ProviderNotificationResponse
+import omnicurekotlin.example.com.appointmentEndpoints.model.Appointment
+import omnicurekotlin.example.com.userEndpoints.model.CommonResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+import java.util.HashMap
+
+interface ApiEndpoints {
+    @POST("loginEndpoints/v1/loginDetails")
+    fun doLogin(@Body body: LoginDetailsRequestBody?): Call<CommonResponse?>?
+
+    // loginWithPassword
+    @POST("loginEndpoints/v1/loginWithPassword")
+    fun loginWithPassword(@Body bodyValues: HashMap<String?, String?>?): Call<CommonResponse?>?
+
+    @POST("healthcareEndpoints/v1/addOrUpdateProviderNotification")
+    fun addOrUpdateNotificationSettings(@Body addNotificationDataRequest: AddNotificationDataRequest?): Call<CommonResponseProviderNotification?>?
+
+    @POST("loginEndpoints/v1/updateFcmkKey")
+    fun commonresponse(@Body body: UpdateFcmkKeyRequestBody?): Call<CommonResponse?>?
+
+    @POST("appointmentEndpoints/v1/addAppointment/{token}")
+    fun addAppointment(
+        @Path("token") token: String?,
+        @Body appointment: Appointment?
+    ): Call<omnicurekotlin.example.com.appointmentEndpoints.model.CommonResponse?>?
+
+    @POST("healthcareEndpoints/v1/getProviderNotification")
+    fun getProviderNotificationDetailsApi(@Body providerId: HashMap<String?, String?>?): Call<ProviderNotificationResponse?>?
+
+    //changed "userId" key to "id"
+    @POST("healthcareEndpoints/v1/getProviderNotification")
+    fun getProviderNotificationDetailsApi(@Body body: ProviderNotificationDetailsRequestBody?): Call<ProviderNotificationResponse?>?
+
+    @POST("loginEndpoints/v1/logout")
+    fun doLogout(@Body body: LogoutRequestBody?): Call<CommonResponse?>?
+
+}
